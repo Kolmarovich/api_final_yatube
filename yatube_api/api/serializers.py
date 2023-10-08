9443faa4b871dda9bcc8bd60b8e899235aa5df6f
Николай Берников <but3rinov@yandex.ru>
2023-10-08 22:26:35 +0300

    1

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
+
+from posts.models import Comment, Follow, Group, Post, User
 
 
-from posts.models import Comment, Post
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = serializers.SlugRelatedField(slug_field='username',
+                                          read_only=True)
 
     class Meta:
         fields = '__all__'
@@ -21,3 +28,38 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('post',)
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
+
+    validators = [
+        UniqueTogetherValidator(
+            queryset=Follow.objects.all(),
+            fields=('user', 'following'),
+            message="Вы уже подписаны на данного автора"
+        )
+    ]
+
+    def validate(self, data):
+        request = self.context.get('request')
+        author = request.user
+        following_user = data.get('following')
+        if author == following_user:
+            raise serializers.ValidationError(
+                'Вы не можете подписаться на самого себя'
+            )
+        return data
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,26 +1,83 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+TRUNCATE_LENGTH = 30
 User = get_user_model()
 
 
-class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+class Group(models.Model):
+    title = models.CharField('Название группы', max_length=200)
+    slug = models.SlugField('Слаг', unique=True)
+    description = models.TextField('Описание')
 
     def __str__(self):
-        return self.text
+        return self.title[:TRUNCATE_LENGTH]
+
+
+class Post(models.Model):
+    text = models.TextField('Текст поста')
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True,
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Автор',
+    )
+    image = models.ImageField(
+        'Картинка', upload_to='posts/', null=True, blank=True
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name='Группа'
+    )
+
+    class Meta:
+        default_related_name = 'posts'
+
+    def __str__(self):
+        return self.text[:TRUNCATE_LENGTH]
 
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User, on_delete=models.CASCADE, verbose_name='Автор'
+    )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
+        Post, on_delete=models.CASCADE, verbose_name='Пост'
+    )
+    text = models.TextField('Текст комментария')
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        'Дата добавления', auto_now_add=True, db_index=True
+    )
+
+    class Meta:
+        default_related_name = 'comments'
+
+    def __str__(self):
+        return (f'Комментарий от {self.author} к посту "{self.post}" :'
+                f' {self.text[:TRUNCATE_LENGTH]}')
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Подписчик',
+        related_name='follower'
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Подписка',
+        related_name='following'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'following'],
+                name='unique_user_following'
+            )
+        ]
